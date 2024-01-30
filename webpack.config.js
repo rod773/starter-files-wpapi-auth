dc90644e548e169927a7845991ec7d4ebe631d03
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-30 07:28:35 +0100

    initial

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+var path = require("path");
+var UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = (env, arg) => {
+  config = {
+    entry: ["./src/index.js"],
+    output: {
+      filename: "bundle.min.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    mode: "development",
+    devServer: {
+      port: 3000,
+      static: path.resolve(__dirname, "dist"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+      ],
+    },
+    optimization: {
+      minimizer: [new UglifyJSPlugin()],
+    },
+  };
+  if (arg.mode === "development") {
+    config.devtool = "source-map";
+  }
+  return config;
+};
+
+/*
+const path = require("path");
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  mode: "development",
+  entry: {
+    main: "./src/main.js",
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 9000,
+  },
+};
+
+
+*/
